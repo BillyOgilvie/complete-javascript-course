707508cdaab7576a3ab4d6dc20e244bbf506a5e8
BillyOgilvie <43520983+BillyOgilvie@users.noreply.github.com>
2020-10-24 11:42:34 +0100

    33 fuctions done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,7 +1,32 @@
-'use strict';
+'use strict'; //this should be the first thing that is in your file!
 
-let hasDriversLicense = false;
+// Example of extra error loggging from use strict
+/* let hasDriversLicense = false;
 const passedTest = true;
 
 if (passedTest) hasDriverLicense = true;
-if (passedTest) console.log('I can drive)');
+if (passedTest) console.log('I can drive)'); */
+
+/*
+=========================================
+----------------Functions----------------
+=========================================
+*/
+/* function logger() {
+    console.log('My name is Bill');
+}
+
+logger();
+logger();
+logger(); */
+
+function fruitProcessor(noOfApples, noOfOranges) {
+    const juice = `Juice with ${noOfApples} apples and ${noOfOranges} oranges`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(5,2);
+console.log(appleOrangeJuice);
