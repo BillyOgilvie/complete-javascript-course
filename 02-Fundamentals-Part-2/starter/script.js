089c1b1038e0811cec5baac4eb76d5794c3921c2
BillyOgilvie <43520983+BillyOgilvie@users.noreply.github.com>
2020-10-24 12:10:15 +0100

    35 done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,9 +8,9 @@ if (passedTest) hasDriverLicense = true;
 if (passedTest) console.log('I can drive)'); */
 
 /*
-=========================================
-----------------Functions----------------
-=========================================
+=============
+--Functions--
+=============
 */
 /* function logger() {
     console.log('My name is Bill');
@@ -60,4 +60,16 @@ console.log(age1, age2); */
 --Arrow Functions--
 ===================
 */
+
+// Arrow function
+const calcAge3 = birthYear => new Date().getFullYear() - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, fisrtName) => {
+   const age = new Date().getFullYear() - birthYear;
+   const retirement = 65 - age;
+   // return retirement;
+   return `${fisrtName} retires in ${retirement} years`
+}
 
