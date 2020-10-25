4fd1bf653c1c72a8e95e31f0c19f89a1743daaf7
BillyOgilvie <43520983+BillyOgilvie@users.noreply.github.com>
2020-10-25 10:38:31 +0000

    Coding Challenge #3 done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -155,7 +155,7 @@ const koalas = [
   [23, 34, 27],
 ];
 
-const calcAverage = scoresArr =>
+/* const calcAverage = scoresArr =>
   scoresArr.reduce((prev, cur) => (prev + cur) / scoresArr.length);
 
 // Test Data 1
@@ -177,25 +177,82 @@ const findWinner = (teamOneAvg, teamTwoAvg) => {
 };
 
 const winner = findWinner(teamOneAvg, teamTwoAvg);
-console.log(winner);
+console.log(winner); */
+
+///////////////////////////////////////
+// Coding Challenge #3
 
 /*
-========================
---ACoding Challenege 2--
-========================
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+GOOD LUCK 😀
 */
 
+/* class Person {
+  constructor(name, massKG, heightM) {
+    (this.name = name), (this.massKG = massKG), (this.heightM = heightM);
+  }
+
+  calcBMI() {
+    return (this.bmi = parseFloat(
+      (this.massKG / this.heightM ** 2).toFixed(1)
+    ));
+  }
+}
+
+const people = [
+  new Person('John Jonas', 105, 1.95),
+  new Person('Mark Miller', 71, 1.7),
+  new Person('Zacky Zane', 92, 1.5),
+  new Person('Sally Smith', 60, 1.7),
+  new Person('Mohammad Mahed', 120, 2.1),
+  new Person('Derek Durk', 62, 1.7),
+  new Person('Boris Bolton', 75, 1.85),
+];
+
+for (const el of people) {
+  el.calcBMI();
+}
+
+let highest = people[0];
+let lowest = people[0];
+people.forEach(el => {
+  if (el.bmi > highest.bmi) {
+    highest = el;
+  } else if (el.bmi < lowest.bmi) {
+    lowest = el;
+  }
+});
+
+console.log(`${highest.name} has the highest BMI, with a BMI of ${highest.bmi}
+Their weight is ${highest.massKG}kg and their height is ${highest.heightM}`);
+console.log(`${lowest.name} has the lowest BMI, with a BMI of ${lowest.bmi}
+Their weight is ${lowest.massKG}kg and their height is ${lowest.heightM}m`); */
+
+///////////////////////////////////////
+// Coding Challenge #4
+
 /*
-Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+Let's improve Steven's tip calculator even more, this time using loops!
 
-1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
-2. And now let's use arrays! So create an array 'bills' containing the test data below.
-3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
-4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
 
-TEST DATA: 125, 555 and 44
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
 
-HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
 
 GOOD LUCK 😀
 */
